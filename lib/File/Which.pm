0ec65cc894517c15dae793668f64a3042677f2c6
Graham Ollis <perl@wdlabs.com>
2015-07-21 08:42:26 -0400

    doco

--- lib/File/Which.pm
+++ lib/File/Which.pm
@@ -257,7 +257,7 @@ sub where {
 
 This module is fully supported back to Perl 5.8.1.  It may work on 5.8.0.  
 It should work on Perl 5.6.x and I may even test on 5.6.2.  I will accept
-patches to maintain compatability for such older Perls, but you may
+patches to maintain compatibility for such older Perls, but you may
 need to fix it on 5.6.x / 5.8.0 and send me a patch.
 
 Not tested on VMS although there is platform specific code
